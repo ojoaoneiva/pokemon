158481bded11492bcb596de4e8366dbb2f458b71
João Neiva <jg.neiva@gmail.com>
2023-10-20 08:40:43 -0300

    env more variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
