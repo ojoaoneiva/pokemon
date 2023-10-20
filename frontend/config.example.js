68fa2c1fdf5888b41e06bb07cba8e6da302073dd
João Gabriel Neiva <122841627+ojoaoneiva@users.noreply.github.com>
2023-10-20 09:12:18 -0300

    Update config.example.js

--- frontend/config.example.js
+++ frontend/config.example.js
@@ -1,1 +1,3 @@
-export const API_URL = 'http://the-IP-where-the-backend-is-running';
+export const API_URL = 'http://the-IP-where-the-backend-is-running:3000';
+
+//exemplo: 'http://190.160.15.125:3000'
